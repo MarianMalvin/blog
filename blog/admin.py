27cd4bce4835889f8c6b9b00a2a94030b1df1def
Ultron <maryan.zozulia@gmail.com>
2019-02-03 21:55:07 +0100

    First commit

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from blog.models import Post, Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_pub')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', post_list, name='posts_list_url'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tag/', tags_lsit, name='tags_list_url')
+    # path('', )
+    # path('', )
+    # path('', )
+    # path('', )
+    # path('', )
+]
