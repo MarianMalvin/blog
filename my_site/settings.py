9ac7169c81f3af6a48612d76fc401ee813876d39
Ultron <maryan.zozulia@gmail.com>
2019-02-13 13:03:41 +0100

    TEMPLATE_CONTEXT_PROCESSORS
    Took 13 minutes

--- my_site/settings.py
+++ my_site/settings.py
@@ -51,6 +51,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'my_site.urls'
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
